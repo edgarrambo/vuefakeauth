e83f96927f3c09329f0b8c85805a57a3e6a69db1
Edgar Alonzo <56144716+edgarrambo@users.noreply.github.com>
2021-11-07 19:34:35 -0600

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="bg-red-800 text-blue-500 font-extrabold">
+<div class="container mx-auto flex items-center justify-between">
+    <!-- added router link to basicrouter icon -->
+    <router-link to="/">
+    <h1 class="tracking-tighter text-3xl font-thin">Basic
+        <span class="font-normal">Router</span>
+    </h1>
+    </router-link>
+    <nav>
+    <ul class="flex space-x-4" >
+        <!-- below are three different ways of routing, all are working -->
+        <!-- notes for if need to reference later -->
+        <router-link to="/">
+        <li class="py-8 px-4 hover:cursor-pointer hover:bg-green-500 hover:text-green-800">Homepage</li></router-link>
+        <router-link :to="{name: 'About'}">
+         <li class="py-8 px-4 hover:cursor-pointer hover:bg-green-500 hover:text-green-800">About</li>
+        </router-link>
+        <router-link :to="{ path: '/hello', name: 'Hello'}">
+        <li class="py-8 px-4 hover:cursor-pointer hover:bg-green-500 hover:text-green-800">Hello</li>
+        </router-link>
+    </ul>
+    </nav>
+</div>
+</div>
+</template>
---
+++ src/pages/404.vue
@@ -0,0 +1,5 @@
+<template>
+    <H1>404 Page Not Found</H1>
+    <h2>Check your self, before you wreck your self</h2>
+    <h2>Check your webpage entered</h2>
+</template>
