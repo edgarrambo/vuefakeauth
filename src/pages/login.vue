c9bdd08e887182fb15532fee67fc8734695c2241
Edgar Alonzo <ragde956@gmail.com>
2021-11-08 21:17:56 -0600

    Added login logic

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,9 +1,22 @@
 <script setup>
 import {ref} from "vue";
+import { useRouter} from "vue-router";
 import useAuth from "../composable/useAuth";
+
+
 const { isAuthenticated, login } = useAuth();
+
 const username = ref("");
 const password = ref("");
+
+const router = useRouter();
+
+const logginIn = () => {
+   login(username.value, password.value);
+   if (isAuthenticated.value) {
+      router.push("/");
+   }
+};
 
 </script>
 
@@ -12,11 +25,11 @@ const password = ref("");
 Logged in: {{ isAuthenticated}}
    <div class="flex items-center justify-center rounded-lg shadow-2xl ">
    <img class="h-64" src="../assets/bglogin.png" alt="Hello Holidays">
-   <form  @submit.prevent="login(username, password)"
+   <form  @submit.prevent="logginIn"
       class="flex flex-col p-4 space-y-4" >
       <input type="text" class="py-2 border-2 rounded-lg" placeholder="Username" v-model="username"/>
        <input type="password" class="py-2 border-2 rounded-lg" placeholder="Password" v-model="password"/>
-       <button type="submit" @submit.prevent="login(username, password)" 
+       <button type="submit" @submit.prevent="logginIn" 
        
        class="py-2 text-indigo-200 bg-indigo-600 rounded-lg" >
        Login
